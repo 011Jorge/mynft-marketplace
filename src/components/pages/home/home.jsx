c928ad308f617a77cc016dec2f67e2f7f7c9aeab
011Jorge <vicentejorge187@gmail.com>
2022-10-10 20:44:32 -0300

    :lipstick: intro completed screen

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
 
+import iconArrow from '../../../img/arrow.svg'
+import iconNft from '../../../img/illustrationNft.svg'
 import Navbar from '../../layout/navbar/navbar'
-import { ContainerHome } from './homeStyles'
+
+import {
+  ContainerHome,
+  ContainerIntroduction,
+  H1,
+  ButtonExplore,
+  ContainerItens,
+  IllustrationNft,
+} from './homeStyles'
 
 function Home() {
   return (
     <ContainerHome>
       <Navbar />
+      <ContainerIntroduction>
+        <ContainerItens>
+          <H1>Discover, collect, and sell extraordinary NFTs</H1>
+          <p>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+          <ButtonExplore>
+            Explore now
+            <div>
+              <img src={iconArrow} alt="icon-arrow" />
+            </div>
+          </ButtonExplore>
+        </ContainerItens>
+        <IllustrationNft src={iconNft} alt="icon-illustration-nft" />
+      </ContainerIntroduction>
     </ContainerHome>
   )
 }
--- src/components/pages/home/homeStyles.js
+++ src/components/pages/home/homeStyles.js
@@ -1,3 +1,89 @@
 import styled from 'styled-components'
 
 export const ContainerHome = styled.div``
+
+export const ContainerIntroduction = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`
+
+export const ContainerItens = styled.button`
+  text-align: left;
+  background-color: transparent;
+  border: none;
+
+  p {
+    width: 600px;
+    height: 66px;
+    margin-top: 25px;
+    margin-bottom: 25px;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 22px;
+    color: #bdcadb;
+  }
+`
+
+export const H1 = styled.h1`
+  width: 600px;
+  height: 96px;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 50px;
+  line-height: 48px;
+  color: #f0f6ff;
+`
+
+export const ButtonExplore = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 4px 4px 4px 24px;
+  gap: 16px;
+  width: 210px;
+  height: 60px;
+  border-radius: 12px;
+  border: none;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  cursor: pointer;
+  color: #ffffff;
+  background: linear-gradient(256.8deg, #0698f9 13.5%, #f906f9 87.25%);
+
+  div {
+    width: 40px;
+    height: 40px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 12px;
+    gap: 10px;
+    background: #fafcff;
+    border-radius: 10px;
+  }
+`
+
+export const IllustrationNft = styled.img`
+  margin-top: 50px;
+
+  animation: moveNFT 3s infinite ease-in-out;
+  animation-direction: alternate;
+
+  @keyframes moveNFT {
+    from {
+      transform: translateY(10%) rotate(5deg);
+    }
+  }
+`
