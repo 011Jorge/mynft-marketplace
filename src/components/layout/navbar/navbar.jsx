1d81b5c218fd2d0a999263c7ee26de98b854c869
011Jorge <vicentejorge187@gmail.com>
2022-10-10 19:13:03 -0300

    :lipstick: stylized navbar

--- src/components/layout/navbar/navbar.jsx
+++ src/components/layout/navbar/navbar.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 
-import Logo from '../../../img/logo.svg'
-import Search from '../../../img/search.svg'
-import ThreePoint from '../../../img/threePoint.svg'
+import iconLogo from '../../../img/logo.svg'
+import iconSearch from '../../../img/search.svg'
+import iconThreePoint from '../../../img/threePoint.svg'
 
-import { ContainerNavbar, ContainerLinks, Link, Button } from './navbarStyles'
+import {
+  ContainerNavbar,
+  Logo,
+  ContainerLinks,
+  Link,
+  ThreePoint,
+  Search,
+  Button,
+} from './navbarStyles'
 
 function Navbar() {
   return (
     <ContainerNavbar>
-      <img src={Logo} alt="icon-logo-mynft" />
+      <Logo src={iconLogo} alt="icon-logo-mynft" />
       <ContainerLinks>
-        <Link href="#">Home</Link>
+        <Link href="#" style={{ fontWeight: 'bolder' }}>
+          Home
+        </Link>
         <Link href="#">Market Place</Link>
         <Link href="#">All NFTs</Link>
         <Link href="#">Activies</Link>
       </ContainerLinks>
-      <img src={ThreePoint} alt="icon-three-point" />
-      <img src={Search} alt="icon-search" />
+      <ThreePoint src={iconThreePoint} alt="icon-three-point" />
+      <Search src={iconSearch} alt="icon-search" />
       <Button>Connect wallet</Button>
     </ContainerNavbar>
   )
--- src/components/layout/navbar/navbarStyles.js
+++ src/components/layout/navbar/navbarStyles.js
@@ -1,9 +1,47 @@
 import styled from 'styled-components'
 
-export const ContainerNavbar = styled.div``
+export const ContainerNavbar = styled.div`
+  margin: 30px;
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+  align-items: center;
+`
+
+export const Logo = styled.img``
 
 export const ContainerLinks = styled.div``
 
-export const Link = styled.a``
+export const Link = styled.a`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  padding: 40px;
+  color: #bdcadb;
+  text-decoration: none;
+`
 
-export const Button = styled.button``
+export const ThreePoint = styled.img`
+  cursor: pointer;
+`
+
+export const Search = styled.img`
+  cursor: pointer;
+`
+
+export const Button = styled.button`
+  width: 170px;
+  height: 44px;
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 24px;
+  color: #bdcadb;
+  border: 3px solid #bdcadb;
+  border-radius: 12px;
+  background-color: transparent;
+  cursor: pointer;
+`
