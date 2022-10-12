2bf54bbd5caa367cc863baba761a70473225e0a4
011Jorge <vicentejorge187@gmail.com>
2022-10-11 21:07:48 -0300

    :lipstick: stylized card

--- src/components/layout/cards/cards.jsx
+++ src/components/layout/cards/cards.jsx
@@ -4,6 +4,8 @@ import PhoneNft from '../../../img/nfts/phoneNft.svg'
 import MonsterNft from '../../../img/nfts/monsterNft.svg'
 import HunterNft from '../../../img/nfts/hunterNft.svg'
 import ToiletNft from '../../../img/nfts/toiletNft.svg'
+import User from '../../../img/itens/user.svg'
+import Heart from '../../../img/itens/heart.svg'
 
 import {
   Container,
@@ -11,6 +13,9 @@ import {
   ImageNft,
   ContainerName,
   ContainerAbout,
+  ContainerDate,
+  UserIcon,
+  HeartIcon,
 } from './cardsStyles'
 
 function Cards() {
@@ -34,16 +39,110 @@ function Cards() {
             <p>Bids: 25</p>
           </div>
         </ContainerAbout>
+        <hr />
+        <ContainerDate>
+          <div>
+            <UserIcon src={User} alt="icon-user" />
+            <p>Artist name</p>
+          </div>
+          <div>
+            <HeartIcon src={Heart} alt="icon-heart" />
+            <p>12121</p>
+          </div>
+        </ContainerDate>
       </Card>
 
       <Card>
         <ImageNft src={MonsterNft} alt="image-phone-nft" />
+        <ContainerName>
+          <p>Collection name</p>
+          <h1>NFT name Ex Shiba...</h1>
+        </ContainerName>
+        <ContainerAbout>
+          <div>
+            <p>Current bid</p>
+            <h1>300 ETH</h1>
+            <p>$130,729.12</p>
+          </div>
+          <div>
+            <p>Auction ending in</p>
+            <h1>03:25h:05m</h1>
+            <p>Bids: 25</p>
+          </div>
+        </ContainerAbout>
+        <hr />
+        <ContainerDate>
+          <div>
+            <UserIcon src={User} alt="icon-user" />
+            <p>Artist name</p>
+          </div>
+          <div>
+            <HeartIcon src={Heart} alt="icon-heart" />
+            <p>12121</p>
+          </div>
+        </ContainerDate>
       </Card>
+
       <Card>
         <ImageNft src={HunterNft} alt="image-phone-nft" />
+        <ContainerName>
+          <p>Collection name</p>
+          <h1>NFT name Ex Shiba...</h1>
+        </ContainerName>
+        <ContainerAbout>
+          <div>
+            <p>Current bid</p>
+            <h1>300 ETH</h1>
+            <p>$130,729.12</p>
+          </div>
+          <div>
+            <p>Auction ending in</p>
+            <h1>03:25h:05m</h1>
+            <p>Bids: 25</p>
+          </div>
+        </ContainerAbout>
+        <hr />
+        <ContainerDate>
+          <div>
+            <UserIcon src={User} alt="icon-user" />
+            <p>Artist name</p>
+          </div>
+          <div>
+            <HeartIcon src={Heart} alt="icon-heart" />
+            <p>12121</p>
+          </div>
+        </ContainerDate>
       </Card>
+
       <Card>
         <ImageNft src={ToiletNft} alt="image-phone-nft" />
+        <ContainerName>
+          <p>Collection name</p>
+          <h1>NFT name Ex Shiba...</h1>
+        </ContainerName>
+        <ContainerAbout>
+          <div>
+            <p>Current bid</p>
+            <h1>300 ETH</h1>
+            <p>$130,729.12</p>
+          </div>
+          <div>
+            <p>Auction ending in</p>
+            <h1>03:25h:05m</h1>
+            <p>Bids: 25</p>
+          </div>
+        </ContainerAbout>
+        <hr />
+        <ContainerDate>
+          <div>
+            <UserIcon src={User} alt="icon-user" />
+            <p>Artist name</p>
+          </div>
+          <div>
+            <HeartIcon src={Heart} alt="icon-heart" />
+            <p>12121</p>
+          </div>
+        </ContainerDate>
       </Card>
     </Container>
   )
--- src/components/layout/cards/cardsStyles.js
+++ src/components/layout/cards/cardsStyles.js
@@ -15,7 +15,7 @@ export const Card = styled.div`
   padding: 16px;
   gap: 16px;
   width: 282px;
-  height: 500px;
+  height: max-content;
   background: rgba(255, 255, 255, 0.6);
   box-shadow: 6px 12px 18px rgba(102, 146, 204, 0.08),
     inset 0px 0px 0.5px rgba(0, 0, 0, 0.1);
@@ -45,6 +45,12 @@ export const Card = styled.div`
     margin-top: 5px;
     margin-bottom: 5px;
   }
+
+  hr {
+    width: 240px;
+    border-radius: 80%;
+    background: linear-gradient(256.8deg, #0698f9 13.5%, #f906f9 87.25%);
+  }
 `
 
 export const ImageNft = styled.img``
@@ -57,4 +63,24 @@ export const ContainerAbout = styled.div`
   display: flex;
   flex-direction: row;
   gap: 40px;
+`
+export const ContainerDate = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 70px;
+
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+`
+
+export const UserIcon = styled.img`
+  padding-right: 5px;
+`
+
+export const HeartIcon = styled.img`
+  padding-right: 5px;
 `
