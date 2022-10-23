1eca72b3213e04d04cb51e63285f7e024a2896e5
011Jorge <vicentejorge187@gmail.com>
2022-10-23 15:20:25 -0300

    :iphone: responsive component categories

--- src/components/layout/categories/categories.jsx
+++ src/components/layout/categories/categories.jsx
@@ -9,21 +9,21 @@ import Category06 from '../../../img/itens/category06.svg'
 import Category07 from '../../../img/itens/category07.svg'
 import Category08 from '../../../img/itens/category08.svg'
 
-import { Container, H1, ContainerImages } from './categoriesStyles'
+import { Container, H1, ContainerImages, Image } from './categoriesStyles'
 
 function Categories() {
   return (
     <Container>
       <H1>Browse by category</H1>
       <ContainerImages>
-        <img src={Category01} alt="image-categories-one" />
-        <img src={Category02} alt="image-categories-two" />
-        <img src={Category03} alt="image-categories-three" />
-        <img src={Category04} alt="image-categories-four" />
-        <img src={Category05} alt="image-categories-five" />
-        <img src={Category06} alt="image-categories-six" />
-        <img src={Category07} alt="image-categories-seven" />
-        <img src={Category08} alt="image-categories-eight" />
+        <Image src={Category01} alt="image-categories-one" />
+        <Image src={Category02} alt="image-categories-two" />
+        <Image src={Category03} alt="image-categories-three" />
+        <Image src={Category04} alt="image-categories-four" />
+        <Image src={Category05} alt="image-categories-five" />
+        <Image src={Category06} alt="image-categories-six" />
+        <Image src={Category07} alt="image-categories-seven" />
+        <Image src={Category08} alt="image-categories-eight" />
       </ContainerImages>
     </Container>
   )
--- src/components/layout/categories/categoriesStyles.js
+++ src/components/layout/categories/categoriesStyles.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  @media (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
 
 export const H1 = styled.h1`
   text-align: center;
@@ -10,9 +16,25 @@ export const H1 = styled.h1`
   font-size: 35px;
   line-height: 48px;
   color: #21204a;
+
+  @media (max-width: 600px) {
+    text-align: center;
+    font-size: 30px;
+  }
+
 `
 
 export const ContainerImages = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+
+  @media (max-width: 600px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+`
+
+export const Image = styled.img`
+  width: 190px;
 `
